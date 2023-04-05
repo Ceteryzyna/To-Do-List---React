0854e192bafecb4bc4c126c33144c034f89ae237
Paula Andrzejewska <haftpaula@gmail.com>
2023-04-05 20:58:39 +0200

    created new files: 'Container', 'Section', changed className, code formatting

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Header from "./Header";
+import Container from "./Container";
+import Section from "./Section";
 
 const tasks = [
-  {id: 1, content: "umyć podłogę", done: true},
-  {id: 2, content: "pomalować paznokcie", done: true},
+  { id: 1, content: "umyć podłogę", done: false },
+  { id: 2, content: "pomalować paznokcie", done: true },
 ];
 const hideDoneTasks = false;
 
 function App() {
   return (
-    <main className="body">
-      <Header title="Lista zadań"/>
-      <Form />
-      <div className="list">
-        <h2 className="tasks__header">Lista zadań</h2>
-        <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
-      </div>
-      <div className="tasks"></div>
-      <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
-</main >
+    <Container>
+      <Header
+        title="Lista zadań"
+      />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+      />
+      <Section
+        title="Lista zadań"
+        headerExtra={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+      />
+    </Container >
   );
 }
 
