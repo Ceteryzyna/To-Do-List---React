c1af90af71e657b9061adfae0af5dd11e21779c2
Paula Andrzejewska <haftpaula@gmail.com>
2023-04-05 11:46:15 +0200

    code formatting

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,13 +1,13 @@
 import "./style.css";
 
-
 const Form = () => (
-    <form className="form">
-        <h2 className="form__header">Dodaj nowe zadanie</h2>
-        <div className="form__input"><input className="form__newTask" placeholder="Co jest do zrobienia?" />
-          <button className="form__button"> Dodaj zadanie</button>
-        </div>
-      </form>
+  <form>
+    <div className="form__input">
+      <input className="form__newTask" placeholder="Co jest do zrobienia?" />
+      <button className="form__button"> Dodaj zadanie
+      </button>
+    </div>
+  </form>
 );
 
 export default Form;
