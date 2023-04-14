181076834fc8438de616a49f87d93db40786d051
Paula Andrzejewska <haftpaula@gmail.com>
2023-04-14 17:56:11 +0200

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -20,6 +20,15 @@ function App() {
   const removeTask = (id) => {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   }
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+
+      return task;
+    }));
+  }
 
   return (
     <Container>
@@ -36,13 +45,16 @@ function App() {
           <Buttons
             tasks={tasks}
             hideDone={hideDone}
-            toggleHideDone={toggleHideDone} />
+            toggleHideDone={toggleHideDone}
+          />
         }
         body={
           <Tasks
             tasks={tasks}
-            hideDone={hideDone} 
-            removeTask={removeTask}/>
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
         }
       />
     </Container >
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,19 +1,22 @@
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone, removeTask }) => (
+const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
 
     <ul className="tasksList">
         {tasks.map(task => (
             <li
                 className={`tasksList__item ${task.done && hideDone ? "tasksList__item--hidden" : ""}`}
                 key={task.id}>
-                <button className=" tasksList__buttonDone">
+                <button
+                    className=" tasksList__buttonDone"
+                    onClick={() => toggleTaskDone(task.id)}
+                >
                     {task.done ? "✔" : ""}
                 </button>
                 <span className={`tasksList__content ${task.done ? "tasksList__content--done" : ""}`}>
                     {task.content}</span>
                 <button className={`tasksList__buttonDone tasksList__buttonRemove  tasksList__buttonRemove`}
-                onClick={() => removeTask(task.id)}
+                    onClick={() => removeTask(task.id)}
                 >
                     🗑
                 </button>
